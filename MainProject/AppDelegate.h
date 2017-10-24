de82279b7811a1f4f9f7dee45cad0d30acc560a2
MacMini <ljy_huan@163.com>
2017-10-24 09:14:08 +0800

    Initial Commit

---
+++ MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by citybao on 2017/10/24.
+//  Copyright © 2017年 citybao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by citybao on 2017/10/24.
+//  Copyright © 2017年 citybao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
